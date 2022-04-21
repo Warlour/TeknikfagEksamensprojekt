27daf7183fa9a0951fed9224a8b11a7fa1b4ebe9
Mathias Kræmer Eberhardt Sørensen (MATH224P) <math224p@zbc.dk>
2022-04-21 10:56:48 +0200

    Added post-processing effects on cameras

--- Eksamensprojekt/Assets/Scripts/ButtonHandler.cs
+++ Eksamensprojekt/Assets/Scripts/ButtonHandler.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class ButtonHandler : MonoBehaviour{
     public Camera[] cameras;
 
+    private void Start(){
+        ChangeCamera(3);
+    }
+
     public void ChangeCamera(int button){
         for (int i = 0; i < cameras.Length; i++){
             if (button-1 != i)
