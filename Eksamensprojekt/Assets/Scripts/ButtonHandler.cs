eee2132c38da6b763d324ab4e96a030a05163e84
Warlour <warlour@eksworld.dk>
2022-04-27 21:32:35 +0200

    Added trees, houses and some furniture

--- Eksamensprojekt/Assets/Scripts/ButtonHandler.cs
+++ Eksamensprojekt/Assets/Scripts/ButtonHandler.cs
@@ -7,15 +7,15 @@ public class ButtonHandler : MonoBehaviour{
     public Camera[] cameras;
 
     private void Start(){
-        ChangeCamera(3);
+        ChangeCamera(0);
     }
 
     public void ChangeCamera(int button){
         for (int i = 0; i < cameras.Length; i++){
-            if (button-1 != i)
+            if (button != i)
                 cameras[i].enabled = false;
             else
-                cameras[i].enabled = true;
+                cameras[button].enabled = true;
         }
     }
 }
