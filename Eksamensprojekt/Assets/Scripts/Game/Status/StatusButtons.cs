8f3907e70a92a88a70136a0efc434566cc2582db
Mathias Kræmer Eberhardt Sørensen (MATH224P) <math224p@zbc.dk>
2022-05-03 14:59:10 +0200

    Added fix buttons to "problems" next step is to implement them

--- Eksamensprojekt/Assets/Scripts/Game/Status/StatusButtons.cs
+++ Eksamensprojekt/Assets/Scripts/Game/Status/StatusButtons.cs
@@ -5,35 +5,73 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class StatusButtons : MonoBehaviour{
+public class StatusButtons : MonoBehaviour {
     public GameObject hsObject;
     public TextMeshProUGUI hsText;
     public GameObject ecPanel;
     public TextMeshProUGUI[] statusvars;
     private bool expanded;
+    public string[] statusnames;
+    public Color32[] Colors;
+    public GameObject indicator;
+    public GameObject indicatordot;
+    private int Matches;
     private void Start (){
         expanded = false;
         ecPanel.SetActive(false);
 
         for (int i = 0; i < statusvars.Length; i++) {
-            statusvars[i].text = "Below Optimal";
-            statusvars[i].color = new Color32(255, 135, 135, 255);
+            statusvars[i].text = statusnames[1];
+            statusvars[i].color = Colors[1];
         }
 
+        Matches = 0;
+        StartCoroutine("indicatorflash");
     }
     
     public void Expandcontract() {
         if (expanded == false) {
             hsText.text = "‹";
             ecPanel.SetActive(true);
-
+            indicatordot.SetActive(false);
         } else {
             hsText.text = "›";
             ecPanel.SetActive(false);
+            indicator.SetActive(false);
         }
         expanded = !expanded;
     }
     public void NextDay() {
         
+    }
+
+    private IEnumerator indicatorflash() {
+        for (; ; ) {
+            for (int i = 0; i < statusnames.Length; i++) {
+                for (int j = 0; j < statusvars.Length; j++) {
+                    if (statusnames[i] != "Optimal" && statusvars[j].text == statusnames[i])
+                        Matches += 1;
+                }
+            }
+            if (Matches > 0) {
+                if (expanded) {
+                    indicatordot.SetActive(false);
+                    indicator.SetActive(!indicator.activeSelf);
+                } else {
+                    indicator.SetActive(false);
+                    indicatordot.SetActive(!indicatordot.activeSelf);
+                }
+            }
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+    public void PressureFix() {
+    
+    }
+    public void O2Fix() {
+    
+    }
+    public void WaterFix() {
+    
     }
 }
